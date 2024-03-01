edc743bb68410da1d8ea3bf99925a85ee6171bbf
NicoleKio <nastiamay.1654@gmail.com>
2024-03-01 06:05:43 +0200

    commit-js-p24-light

--- summary/lesson-5/functional_expressions.js
+++ summary/lesson-5/functional_expressions.js
@@ -24,10 +24,30 @@ console.log(calc(12, 15));
 
 
 let calc2 = (q, w) => q + w;
-console.log(calc2())
+console.log(calc2(45, 10));
+
+
+// let asd = () => console.log(arguments);            //в стрілочних не існує arguments
+// console.log(asd());
 
 
 
+
+//---------------------------------------------------- THIS ---------------------------------------------------------------
+
+// референс на об'єкт
+
+let user = {
+    name: 'Lilith',
+    age: 27,
+    greeting2: function (msg) {
+        return `${msg}, my name is ${this.name}`
+    },
+    alsoGreet: (msg) => `${msg}, my age is ${user.age}`
+    // в стрілочних контекст this втрачається, бо стрФ посилається на глобальний ел. window
+}
+console.log(user.greeting2('Greetings'));
+console.log(user.alsoGreet('Additionally'))
 
 
 
