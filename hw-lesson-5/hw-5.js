0383c70505a049f0154ea3dd60c92a9d764580a0
NicoleKio <nastiamay.1654@gmail.com>
2024-03-02 18:16:23 +0200

    commit-js-p24-light

--- hw-lesson-5/hw-5.js
+++ hw-lesson-5/hw-5.js
@@ -1,8 +1,112 @@
 //
 // ВСЕ ЗРОБИТИ СТРІЛОЧНИМИ ФУНКЦІЯМИ!
-//     - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б / S = a ⋅ b
 
-let calcS = (a, b) => a + b;
-console.log(calcS(52, 9));
+const calcS = (a, b) => a * b;
+console.log(calcS(11, 10))
+
+
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r / S = π r2
+
+const Pi = 3.14;
+const calcS2 = r => Pi * r *  2;
+console.log(calcS2(50, 66));
+
+
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r / S = 2πRH
+
+const calcS3 = (r, h) => 2 * Pi * r * h;
+console.log(calcS3(45, 95));
+
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+let people = [
+    {name: 'Caroline', age: 24, status: false},
+    {name: 'Amanda', age: 36, status: false},
+    {name: 'Eric', age: 20, status: true},
+    {name: 'Monalyne', age: 16, status: true},
+    {name: 'Lyam', age: 22, status: true},
+    {name: 'Joseph', age: 39, status: false},
+    {name: 'Beatrice', age: 19, status: true},
+]
+
+const arrOfPeople = (arr) => {
+    for (let arrElement of arr) {
+        console.log(`name - ${arrElement.name}, age - ${arrElement.age}, status - ${arrElement.status}`)
+    }
+}
+
+arrOfPeople(people);
+
+
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+const textS = liText => {
+    document.write(`<p>${liText}</p>`)
+
+}
+textS('Paradise for Nothing')
+
+
+
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+const ulList = textLi2 => {
+    document.write(`<ul>
+                        <li>${textLi2}</li>
+                        <li>${textLi2}</li>
+                        <li>${textLi2}</li>
+                   </ul>`)
+}
+
+ulList('Text Kio')
+
+
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+
+
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+
+
+
+
+// - створити функцію яка повертає найменьше число з масиву
+
+
+
+// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+
+
+
+
+
+
+
+
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+
+
+
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+
+
+
 
 
