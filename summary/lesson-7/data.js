1321722c57f53a8a6026cff94c16c4f1862a7097
NicoleKio <nastiamay.1654@gmail.com>
2024-03-08 21:05:52 +0200

    commit-js-p35-light

--- summary/lesson-7/data.js
+++ summary/lesson-7/data.js
@@ -19,14 +19,28 @@ console.log(dataCr)
 
 
 
+//------------------------------------------------------- ОБРОБКА ПОМИЛОК --------------------------------------------------------------------
+
+console.log('start')
+    try{
+        console.log(a);
+    } catch (e) {
+        console.log(e)
+    }finally {
+        console.log('Kio')
+    }
+
+console.log('end')
 
 
+function calc (a, b){
+    if (b === 0){
+        throw new Error('b is 0')
+    }
+    return a/ b;
+}
 
-
-
-
-
-
+calc(4, 0);
 
 
 
