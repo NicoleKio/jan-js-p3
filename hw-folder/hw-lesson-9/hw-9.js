fcf0a1d2a62c76a6c1472f378c12a2185cc72798
NicoleKio <nastiamay.1654@gmail.com>
2024-03-17 22:39:05 +0200

    commit-js-p55-light

--- hw-folder/hw-lesson-9/hw-9.js
+++ hw-folder/hw-lesson-9/hw-9.js
@@ -137,16 +137,137 @@ let simpsons = [
 for (const simpson of simpsons) {
     let divSimpson = document.createElement('div')
     divSimpson.classList.add('member')
+    let personS = document.createElement('h2')
+    personS.innerText = simpson.name + simpson.surname;
+    let ageS = document.createElement('h3')
+    ageS.innerText = `Age: ${simpson.age}`;
+    let infoS = document.createElement('p')
+    infoS.innerText = simpson.info;
+    let photoS = document.createElement('img')
+    photoS.setAttribute('src', simpson.photo)
 
-
+    divSimpson.append(personS, ageS, infoS, photoS)
     document.body.appendChild(divSimpson)
 }
-
-
 
 // <!--    =========================-->
 // <!--    Цикл в циклі-->
 // <!--    - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)-->
-//
 // <!--    Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами-->
 // <!--    Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом-->
+
+//title, monthDuration, hourDuration, modules
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+
+for (const coursesA of coursesArray) {
+    let divA = document.createElement('div');
+
+    let ulA = document.createElement('ul')
+
+    for (const courseD of coursesA) {
+        let divB = document.createElement('')
+
+        let titleB = document.createElement('h2');
+        titleB.innerText = courseD.title;
+
+        let monthDurationB = document.createElement('h3')
+        monthDurationB.innerText = `MonthDuration - ${courseD.monthDuration}, hourDuration - ${courseD.hourDuration}`;
+
+        let modulesB = document.createElement('li');
+        modulesB.innerText = courseD.modules;
+
+        divB.append(titleB, modulesB, modulesB)
+    }
+
+
+    ulA.appendChild(liB)
+    divA.appendChild(coursesA);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
